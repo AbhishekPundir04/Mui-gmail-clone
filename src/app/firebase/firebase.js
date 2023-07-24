f093f22c99e32dc950de2247f82741518a6b9ec6
AbhishekPundir04 <pundirabhishek471@gmail.com>
2023-07-24 16:56:36 +0530

    added firebase

---
+++ src/app/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2MiNaRtjLtXTFLr0CY-3KIRB3jJb3dCA",
+  authDomain: "clone-with-react-85303.firebaseapp.com",
+  projectId: "clone-with-react-85303",
+  storageBucket: "clone-with-react-85303.appspot.com",
+  messagingSenderId: "902873947988",
+  appId: "1:902873947988:web:a90449d5e58c50973f72c1",
+  measurementId: "G-S55PZ4PH53",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export { db };
